2fcd3907b0ced15343a9832c6aca2549cac9131d
sibaprksh <siba4269@gmail.com>
2021-05-06 16:38:22 +0530

    1 file modified

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,19 +8,20 @@ import { authActions } from "../../actions";
 import "./Home.css";
 
 const Navigation = props => {
-  console.log("Navigation");
-  console.log({ props });
+  //console.log("Navigation");
+  //console.log({ props });
 
-  const { state, current, size } = props;
+  const { state, current, size } = props; // step properties
 
-  const isInvalidSelected = !state.selected?.length;
+  const isInvalidSelected = current == 1 && !state.selected?.length;
+  const isInvalidSlot = current == 2 && !state.slots?.length;
 
   return (
-    <div>
+    <div className="pt-5">
       {current !== size ? (
         <button
           onClick={props.next}
-          disabled={isInvalidSelected}
+          disabled={isInvalidSelected || isInvalidSlot}
           className="btn btn-primary"
         >
           Next
@@ -30,8 +31,24 @@ const Navigation = props => {
   );
 };
 
+const Before = props => {
+  return (
+    <div>
+      {props.current !== 1 ? (
+        <button className="btn btn-warning" onClick={props.prev}>
+          Back
+        </button>
+      ) : null}
+    </div>
+  );
+};
+
+const After = props => {
+  return <></>;
+};
+
 const SelectStep = props => {
-  props.jump(2); // jump to step
+  //props.next(); // jump to next step
 
   const dispatch = useDispatch();
   const history = useHistory();
@@ -51,7 +68,13 @@ const SelectStep = props => {
   ]);
 
   const [search, setSearch] = useState("");
-  const [selected, setSelected] = useState(props.state?.selected || []);
+  const [selected, setSelected] = useState(
+    props.state.selected || [
+      { name: ".NET MVC" },
+      { name: "AI" },
+      { name: "Spark" }
+    ]
+  );
 
   useEffect(() => {
     props.setState("selected", [...selected]);
@@ -101,7 +124,10 @@ const SelectStep = props => {
 
   return (
     <>
-      <div className="pt-5">
+      <div className="pt-0">
+        <div className="text-center pb-3">
+          <h4>Select Skills!</h4>
+        </div>
         <div className="form-group has-search">
           <span className="fa fa-search form-control-feedback" />
           <input
@@ -114,46 +140,68 @@ const SelectStep = props => {
           />
         </div>
         <div>{selectedItems}</div>
-        <div className="grid-container pt-5 pb-5">{listItems}</div>
+        <div className="grid-container pt-5">{listItems}</div>
       </div>
     </>
   );
 };
 
 const TimeStep = props => {
-  const defaultSlot = { date: "", "start-time": "", "end-time": "" };
-  const [slots, setSlots] = useState([defaultSlot]);
+  //props.next(); // jump to next Step
+
+  const slotTemplate = {
+    date: "2021-05-07",
+    "start-time": "10:00",
+    "end-time": "23:00"
+  };
+
+  const [slots, setSlots] = useState(
+    props.state?.slots?.length ? props.state.slots : [slotTemplate]
+  );
+
+  useEffect(() => {
+    const s = slots.filter(s => s.date); // filter out invalid dates
+    props.setState("slots", s);
+  }, [slots]);
+
+  const handleChange = index => e => {
+    const {
+      target: { name, value }
+    } = e;
+    setSlots(
+      slots.map((item, i) => (i === index ? { ...item, [name]: value } : item))
+    );
+  };
 
   return (
     <>
-      {/* First Name
-      <input
-        type="text"
-        name="name"
-        value={props.getState("name", "aa")}
-        onChange={props.handleChange}
-      /> */}
+      <div className="text-center pb-3">
+        <h4>Add Time Slots!</h4>
+      </div>
       {slots.map((slot, index) => {
         return (
-          <div className="slot-container pb-2 pt-2">
+          <div className="slot-container pb-2 pt-2" key={index}>
             <input
               type="date"
               placeholder="Date"
               name="date"
               value={slot.date}
               className="form-control"
+              onChange={handleChange(index)}
             />
             <input
               type="time"
               name="start-time"
               value={slot["start-time"]}
               className="form-control"
+              onChange={handleChange(index)}
             />
             <input
               type="time"
               name="end-time"
               value={slot["end-time"]}
               className="form-control"
+              onChange={handleChange(index)}
             />
             {slots.length != 1 ? (
               <button
@@ -174,7 +222,7 @@ const TimeStep = props => {
 
       <button
         className="mb-2 btn btn-primary"
-        onClick={() => setSlots(s => [...s, defaultSlot])}
+        onClick={() => setSlots(s => [...s, slotTemplate])}
       >
         Add
       </button>
@@ -183,28 +231,36 @@ const TimeStep = props => {
 };
 
 const FinalStep = props => {
-  console.log({ props });
-  const handleSubmit = () => console.log(props);
+  const handleSubmit = () => console.log(props.state);
+
   return (
     <div>
-      <b>Data:</b> {props.state.name}
-      <button type="button" onClick={handleSubmit}>
+      <div className="text-center pb-3">
+        <h4>Preview!</h4>
+      </div>
+      <b>Skils:</b>
+      <ol>
+        {props.state.selected.map((s, index) => (
+          <li key={index}>{s.name}</li>
+        ))}
+      </ol>
+      <b>Slots:</b>
+      <ol>
+        {props.state.slots?.map((s, index) => (
+          <li key={index}>
+            {s.date} : {s["start-time"]} - {s["end-time"]}
+          </li>
+        ))}
+      </ol>
+      <button
+        type="button"
+        onClick={handleSubmit}
+        className="btn btn-primary mt-5"
+      >
         Submit
       </button>
     </div>
   );
-};
-
-const Before = props => {
-  return (
-    <div>
-      {props.current !== 1 ? <button onClick={props.prev}>Back</button> : null}
-    </div>
-  );
-};
-
-const After = props => {
-  return <></>;
 };
 
 export default function Home() {
